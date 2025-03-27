0ba520fea29c2fdc942ef6ebdf697aa0c9e53383
iSeFz <seifyahia@gmail.com>
2025-03-28 01:36:00 +0200

    Separated item and product into models

---
+++ elevate/lib/models/product.dart
@@ -0,0 +1,15 @@
+// Model class for the product object
+class Product {
+  final String image;
+  final String name;
+  final String brand;
+  final String price;
+
+  // Default constructor
+  Product({
+    required this.image,
+    required this.name,
+    required this.brand,
+    required this.price,
+  });
+}
