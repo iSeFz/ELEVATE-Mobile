47ccaa9781714f5542f53b0eba5bddac0604bb9c
iSeFz <seifyahia@gmail.com>
2025-03-28 06:19:19 +0200

    Made the necessary modifications to config files Updated the used java, kotlin and NDK versions Added the necessary packages for Firebase and Google Services Updated the dependencies of the whole project

--- elevate/android/app/build.gradle.kts
+++ elevate/android/app/build.gradle.kts
@@ -3,20 +3,34 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    // Add the Google Services plugin.
+    id("com.google.gms.google-services")
+}
+
+dependencies {
+  // Import the Firebase BoM (Bill of Materials)
+  implementation(platform("com.google.firebase:firebase-bom:33.11.0"))
+
+  // When using the BoM, don't specify versions in Firebase dependencies
+  implementation("com.google.firebase:firebase-analytics")
+
+  // Add the dependencies for other desired Firebase products
+  implementation("com.google.firebase:firebase-firestore")
+  implementation("com.google.firebase:firebase-auth")
 }
 
 android {
     namespace = "com.example.elevate"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
 
     defaultConfig {
@@ -24,7 +38,7 @@ android {
         applicationId = "com.example.elevate"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
