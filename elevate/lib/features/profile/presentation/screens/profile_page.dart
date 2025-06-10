d3499c332b5546bcad079dd8346c505781711a0f
iSeFz <seifyahia@gmail.com>
2025-06-10 18:10:14 +0300

    Completely updated the edit profile page to use the new profile cubit Shared the profile cubit between profile and edit profile pages using BlocProvider.value Used pre-defined CustomTextFormField instead of the redundant _buildEditableTextField method

--- elevate/lib/features/profile/presentation/screens/profile_page.dart
+++ elevate/lib/features/profile/presentation/screens/profile_page.dart
@@ -22,208 +22,220 @@ class ProfilePage extends StatelessWidget {
 
     return BlocProvider(
       create: (context) => ProfileCubit()..initializeCustomer(customer),
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          toolbarHeight: 70,
-          centerTitle: true,
-          backgroundColor: Colors.white,
-          shadowColor: Colors.black26,
-          title: Text(
-            'Profile',
-            style: TextStyle(
-              color: theme.colorScheme.secondary,
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      child: Builder(
+        builder: (context) {
+          final profileCubit = context.read<ProfileCubit>();
+
+          return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              toolbarHeight: 70,
+              centerTitle: true,
+              backgroundColor: Colors.white,
+              shadowColor: Colors.black26,
+              title: Text(
+                'Profile',
+                style: TextStyle(
+                  color: theme.colorScheme.secondary,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
-            textAlign: TextAlign.center,
-          ),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: screenWidth * 0.07,
-              vertical: screenHeight * 0.01,
-            ),
-            child: Column(
-              children: [
-                // Customer profile photo
-                CircleAvatar(
-                  radius: screenWidth * 0.2,
-                  backgroundColor: theme.colorScheme.tertiary,
-                  child: Icon(
-                    Icons.person,
-                    size: screenWidth * 0.2,
-                    color: theme.colorScheme.onPrimaryContainer,
-                  ),
+            body: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: screenWidth * 0.07,
+                  vertical: screenHeight * 0.01,
                 ),
-                SizedBox(height: screenHeight * 0.02),
-                // Customer name field
-                BlocBuilder<ProfileCubit, ProfileState>(
-                  builder: (context, state) {
-                    String displayName =
-                        state is ProfileLoaded
-                            ? context.read<ProfileCubit>().fullName
-                            : 'User Name';
-                    return Text(
-                      displayName,
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                        color: theme.textTheme.titleLarge?.color,
-                      ),
-                    );
-                  },
-                ),
-                SizedBox(height: screenHeight * 0.015),
-                // Container for loyalty points
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: screenWidth * 0.04,
-                    vertical: screenHeight * 0.01,
-                  ),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFECECEC),
-                    borderRadius: BorderRadius.circular(12),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.shade200,
-                        offset: const Offset(0, 2),
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Icon(
-                        Icons.star_rounded,
-                        size: 24,
-                        color: Color(0xFFFFD700),
-                      ),
-                      SizedBox(width: screenWidth * 0.02),
-                      BlocBuilder<ProfileCubit, ProfileState>(
-                        builder: (context, state) {
-                          String displayPoints =
-                              state is ProfileLoaded
-                                  ? context.read<ProfileCubit>().loyaltyPoints
-                                  : '0 Points';
-                          return Text(
-                            displayPoints,
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: theme.colorScheme.secondary,
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: screenHeight * 0.04),
-                // Container for the list of profile options
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey[100],
-                    borderRadius: BorderRadius.circular(5),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.shade200,
-                        spreadRadius: 2,
-                        blurRadius: 5,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: ListView(
-                    shrinkWrap: true,
-                    padding: EdgeInsets.symmetric(
-                      vertical: screenHeight * 0.005,
-                    ),
-                    children: <Widget>[
-                      ProfilePageOption(
-                        icon: Icons.edit_outlined,
-                        text: 'Edit Profile',
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const EditProfilePage(),
-                            ),
-                          );
-                        },
-                      ),
-                      ProfilePageOption(
-                        icon: Icons.lock_outline_rounded,
-                        text: 'Change Password',
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const ChangePasswordPage(),
-                            ),
-                          );
-                        },
-                      ),
-                      ProfilePageOption(
-                        icon: Icons.history_outlined,
-                        text: 'Order History',
-                        onTap: () {
-                          // TODO: Implement Order History navigation/action
-                        },
-                      ),
-                      ProfilePageOption(
-                        icon: Icons.settings_outlined,
-                        text: 'Settings',
-                        onTap: () {
-                          // TODO: Implement Settings navigation/action
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(height: screenHeight * 0.03),
-                // Logout Button
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton.icon(
-                    icon: Icon(
-                      Icons.logout,
-                      color: Colors.white,
-                      size: screenWidth * 0.06,
-                    ),
-                    label: const Text(
-                      'Logout',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                child: Column(
+                  children: [
+                    // Customer profile photo
+                    CircleAvatar(
+                      radius: screenWidth * 0.2,
+                      backgroundColor: theme.colorScheme.tertiary,
+                      child: Icon(
+                        Icons.person,
+                        size: screenWidth * 0.2,
+                        color: theme.colorScheme.onPrimaryContainer,
                       ),
                     ),
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: theme.colorScheme.secondary,
+                    SizedBox(height: screenHeight * 0.02),
+                    // Customer name field
+                    BlocBuilder<ProfileCubit, ProfileState>(
+                      builder: (context, state) {
+                        String displayName =
+                            (state is ProfileLoaded || state is ProfileUpdated)
+                                ? profileCubit.fullName
+                                : 'User Name';
+                        return Text(
+                          displayName,
+                          style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                            color: theme.textTheme.titleLarge?.color,
+                          ),
+                        );
+                      },
+                    ),
+                    SizedBox(height: screenHeight * 0.015),
+                    // Container for loyalty points
+                    Container(
                       padding: EdgeInsets.symmetric(
-                        vertical: screenHeight * 0.015,
+                        horizontal: screenWidth * 0.04,
+                        vertical: screenHeight * 0.01,
                       ),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5),
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFECECEC),
+                        borderRadius: BorderRadius.circular(12),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.shade200,
+                            offset: const Offset(0, 2),
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(
+                            Icons.star_rounded,
+                            size: 24,
+                            color: Color(0xFFFFD700),
+                          ),
+                          SizedBox(width: screenWidth * 0.02),
+                          BlocBuilder<ProfileCubit, ProfileState>(
+                            builder: (context, state) {
+                              String displayPoints =
+                                  (state is ProfileLoaded ||
+                                          state is ProfileUpdated)
+                                      ? profileCubit.loyaltyPoints
+                                      : '0 Points';
+                              return Text(
+                                displayPoints,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: theme.colorScheme.secondary,
+                                ),
+                              );
+                            },
+                          ),
+                        ],
                       ),
                     ),
-                    onPressed: () {
-                      signOutGoogle();
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const LoginPage(),
+                    SizedBox(height: screenHeight * 0.04),
+                    // Container for the list of profile options
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey[100],
+                        borderRadius: BorderRadius.circular(5),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.shade200,
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child: ListView(
+                        shrinkWrap: true,
+                        padding: EdgeInsets.symmetric(
+                          vertical: screenHeight * 0.005,
                         ),
-                      );
-                    },
-                  ),
+                        children: [
+                          ProfilePageOption(
+                            icon: Icons.edit_outlined,
+                            text: 'Edit Profile',
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder:
+                                      (context) => BlocProvider.value(
+                                        value: profileCubit,
+                                        child: const EditProfilePage(),
+                                      ),
+                                ),
+                              );
+                            },
+                          ),
+                          ProfilePageOption(
+                            icon: Icons.lock_outline_rounded,
+                            text: 'Change Password',
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder:
+                                      (context) => const ChangePasswordPage(),
+                                ),
+                              );
+                            },
+                          ),
+                          ProfilePageOption(
+                            icon: Icons.history_outlined,
+                            text: 'Order History',
+                            onTap: () {
+                              // TODO: Implement Order History navigation/action
+                            },
+                          ),
+                          ProfilePageOption(
+                            icon: Icons.settings_outlined,
+                            text: 'Settings',
+                            onTap: () {
+                              // TODO: Implement Settings navigation/action
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: screenHeight * 0.03),
+                    // Logout Button
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        icon: Icon(
+                          Icons.logout,
+                          color: Colors.white,
+                          size: screenWidth * 0.06,
+                        ),
+                        label: const Text(
+                          'Logout',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: theme.colorScheme.secondary,
+                          padding: EdgeInsets.symmetric(
+                            vertical: screenHeight * 0.015,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                        onPressed: () {
+                          signOutGoogle();
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginPage(),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
