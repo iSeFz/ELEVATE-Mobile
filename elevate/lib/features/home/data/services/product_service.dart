aa2562953cc9051ff5341832b83ae73d8edd2a7e
Nour Tarek <nourtarrek.2@gmail.com>
2025-06-24 16:37:42 +0300

    Update product_service.dart

--- elevate/lib/features/home/data/services/product_service.dart
+++ elevate/lib/features/home/data/services/product_service.dart
@@ -8,8 +8,6 @@ import '../models/product_details_model.dart';
 class ProductService {
   static String baseUrl = "https://elevate-gp.vercel.app/api/v1/";
 
-  // ProductService({required this.baseUrl});
-
   static Future<List<ProductCardModel>> getAllProductsCards() async {
     final response = await http.get(Uri.parse('$baseUrl/products'));
 
@@ -33,51 +31,5 @@ class ProductService {
       throw Exception('Failed to load product details');
     }
   }
-
-  // XXSmall
-  // XSmall
-  // Small
-  // Medium
-  // Large
-  // XLarge
-  // XXLarge
-  static Future<List<String>?> getAllSizes () async {
-    final response = await http.get(Uri.parse('$baseUrl/products/sizes'));
-
-    if (response.statusCode == 200) {
-      // final Map<String, dynamic> jsonResponse = json.decode(response.body);
-      // final List<String> fullSizes = List<String>.from(
-      //     jsonResponse['data']['sizes'] ?? []);
-      // List<String> shortSizes = [];
-      //   for (var size in fullSizes) {
-      //     final regex = RegExp(r'^(X+)?(.*)$', caseSensitive: false);
-      //     final match = regex.firstMatch(size);
-      //     if (match != null) {
-      //       final xPart = match.group(1) ?? '';
-      //       final afterX = match.group(2)?.trim() ?? '';
-      //       final firstLetter = afterX.isNotEmpty
-      //           ? afterX[0].toUpperCase()
-      //           : '';
-      //       shortSizes.add('$xPart$firstLetter');
-      //     } else {
-      //       shortSizes.add(size); // fallback
-      //     }
-      //   }
-      //   return shortSizes;
-      }
-      else {
-        throw Exception('Failed to load product details');
-      }
-  }
-
-    // String? getVariantIdBySize(String shortLabel, ) {
-    //   final match = sizeMap.entries.firstWhere(
-    //         (entry) => entry.value == shortLabel,
-    //     orElse: () => const MapEntry('', ''),
-    //   );
-    //   return match.key.isEmpty ? null : match.key;
-    // }
-
-
 
   }
