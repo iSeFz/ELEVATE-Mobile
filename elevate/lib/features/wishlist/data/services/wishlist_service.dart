9a5e220acd21106c8400d8143e2fc2925803b196
iSeFz <seifyahia@gmail.com>
2025-06-30 15:12:03 +0300

    Add to wishlist feature implementation Added new state and new method to the cubit to handle the addition process Updated the wishlist page to show a new empty wishlist layout

--- elevate/lib/features/wishlist/data/services/wishlist_service.dart
+++ elevate/lib/features/wishlist/data/services/wishlist_service.dart
@@ -4,10 +4,11 @@ import '../../../../core/constants/constants.dart';
 import '../../../product_details/data/models/product_card_model.dart';
 
 class WishlistService {
-  Future<List<ProductCardModel>> fetchWishlist(String userID) async {
+  // Fetch the wishlist for a specific customer
+  Future<List<ProductCardModel>> fetchWishlist(String customerID) async {
     try {
       final response = await http.get(
-        Uri.parse("$apiBaseURL/v1/customers/me/wishlist?userId=$userID"),
+        Uri.parse("$apiBaseURL/v1/customers/me/wishlist?userId=$customerID"),
         headers: {testAuthHeader: testAuthValue},
       );
 
@@ -26,11 +27,43 @@ class WishlistService {
     }
   }
 
-  Future<bool> removeFromWishlist(String userID, String productID) async {
+  // Add a product to the wishlist
+  Future<ProductCardModel?> addToWishlist(
+    String customerID,
+    String productID,
+  ) async {
+    try {
+      final response = await http.post(
+        Uri.parse("$apiBaseURL/v1/customers/me/wishlist/items?userId=$customerID"),
+        headers: {
+          testAuthHeader: testAuthValue,
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({'productId': productID}),
+      );
+
+      final responseData = json.decode(response.body);
+
+      // Return the added product if the addition was done successfully
+      if (response.statusCode == 200 && responseData['status'] == 'success') {
+        final List<dynamic> productsJson = responseData['data'];
+        // Return the first product in the returned wishlist
+        return ProductCardModel.fromJson(productsJson[0]);
+      }
+
+      // Otherwise, return null stating that something went wrong
+      return null;
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  // Remove a product from the wishlist
+  Future<bool> removeFromWishlist(String customerID, String productID) async {
     try {
       final response = await http.delete(
         Uri.parse(
-          "$apiBaseURL/v1/customers/me/wishlist/items/$productID?userId=$userID",
+          "$apiBaseURL/v1/customers/me/wishlist/items/$productID?userId=$customerID",
         ),
         headers: {testAuthHeader: testAuthValue},
       );
--- elevate/lib/features/wishlist/presentation/screens/wishlist_page.dart
+++ elevate/lib/features/wishlist/presentation/screens/wishlist_page.dart
@@ -11,10 +11,15 @@ class WishlistPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final theme = Theme.of(context);
+
     return BlocProvider<WishlistCubit>(
       create: (context) => WishlistCubit()..fetchWishlist(userID),
       child: Scaffold(
         appBar: AppBar(
+          automaticallyImplyLeading: false,
           title: Text(
             'Wishlist',
             style: TextStyle(fontWeight: FontWeight.bold),
@@ -23,68 +28,101 @@ class WishlistPage extends StatelessWidget {
         ),
         body: BlocConsumer<WishlistCubit, WishlistState>(
           listener: (context, state) {
-            if (state is WishlistItemRemoved) {
+            if (state is WishlistProductRemoved) {
               ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(content: Text('Item removed from wishlist')),
+                SnackBar(content: Text('Product removed from wishlist')),
+              );
+            } else if (state is WishlistProductAdded) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Product added to wishlist')),
               );
             } else if (state is WishlistError) {
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
                   content: Text(state.message),
-                  backgroundColor: Theme.of(context).primaryColor,
+                  backgroundColor: theme.primaryColor,
                 ),
               );
             }
           },
           builder: (context, state) {
+            final wishlistProducts =
+                context.read<WishlistCubit>().wishlistProducts;
             if (state is WishlistLoading) {
-              return Center(
-                child: CircularProgressIndicator(
-                  color: Theme.of(context).primaryColor,
-                ),
-              );
-            } else if (state is WishlistLoaded) {
-              final wishlistProducts =
-                  context.read<WishlistCubit>().wishlistProducts;
-
-              return Padding(
-                padding: const EdgeInsets.all(8),
-                child: ListView.builder(
-                  itemCount: (wishlistProducts.length / 2).ceil(),
-                  itemBuilder: (context, index) {
-                    final int firstIndex = index * 2;
-                    final int secondIndex = firstIndex + 1;
-                    final double screenTotalWidth =
-                        MediaQuery.of(context).size.width;
-
-                    return Row(
-                      children: [
-                        ProductCard(
-                          userId: userID,
-                          product: wishlistProducts[firstIndex],
-                        ),
-                        if (secondIndex < wishlistProducts.length)
-                          ProductCard(
-                            userId: userID,
-                            product: wishlistProducts[secondIndex],
-                          )
-                        else
-                          SizedBox(width: screenTotalWidth / 2 - 8),
-                      ],
-                    );
-                  },
-                ),
-              );
+              return Center(child: CircularProgressIndicator());
             } else if (state is WishlistEmpty) {
               return Center(
-                child: Text(
-                  'No wishlist item found.',
-                  style: TextStyle(fontSize: 20),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.shopping_bag_outlined,
+                      size: screenWidth * 0.3,
+                      color: theme.colorScheme.primary,
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    Text(
+                      'Nothing added to wishlist yet.\nStart shopping now!',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: screenHeight * 0.02),
+                    ElevatedButton(
+                      onPressed: () {
+                        // TODO: Navigate to the home page via the main page
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: theme.colorScheme.primary,
+                        padding: EdgeInsets.symmetric(
+                          vertical: screenHeight * 0.015,
+                          horizontal: screenWidth * 0.15,
+                        ),
+                        textStyle: TextStyle(
+                          fontSize: screenWidth * 0.04,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),
+                        ),
+                      ),
+                      child: Text(
+                        'Continue Shopping',
+                        style: TextStyle(color: theme.colorScheme.onPrimary),
+                      ),
+                    ),
+                  ],
                 ),
               );
             }
-            return Center(
-              child: Text('Something went wrong. Please try again.'),
+            // Finally if the wishlist is loaded and is not empty, display the products
+            return Padding(
+              padding: const EdgeInsets.all(8),
+              child: ListView.builder(
+                itemCount: (wishlistProducts.length / 2).ceil(),
+                itemBuilder: (context, index) {
+                  final int firstIndex = index * 2;
+                  final int secondIndex = firstIndex + 1;
+
+                  return Row(
+                    children: [
+                      ProductCard(
+                        userId: userID,
+                        product: wishlistProducts[firstIndex],
+                      ),
+                      if (secondIndex < wishlistProducts.length)
+                        ProductCard(
+                          userId: userID,
+                          product: wishlistProducts[secondIndex],
+                        )
+                      else
+                        SizedBox(width: screenWidth / 2 - 8),
+                    ],
+                  );
+                },
+              ),
             );
           },
         ),
